d7e250efaf2800ea93f17f4f7b3f936eac045a89
Matthew Redd <mredd@deseretbook.com>
2015-11-15 20:35:10 -0700

    Innitial commit of 2 simple ruby files for interpreting a binary byte in ascii, and converting text into its binary representation.  To be used in educational demos.

---
+++ bytewriter.rb
@@ -0,0 +1,18 @@
+#!/Users/mredd/.rvm/rubies/ruby-1.9.3-p448/bin/ruby
+#
+
+puts "ByteWriter...."
+
+byte = ARGV[0].to_s
+
+puts byte
+
+byte = Integer ('0b' + byte)
+
+puts "base 10: " + byte.to_s
+puts "base 2: " + byte.to_s(2)
+puts "Ascii Character: " + byte.chr
+
+
+
+
---
+++ nameinbinary.rb
@@ -0,0 +1,28 @@
+#!/Users/mredd/.rvm/rubies/ruby-1.9.3-p448/bin/ruby
+#
+
+puts "ByteWriter...."
+
+name = ARGV[0].to_s
+
+puts name 
+
+name.each_char do |c|
+  puts c + ' - ' + c.getbyte(0).to_s(2)
+end
+
+name.each_byte do |c|
+  puts c.to_s(2)
+end
+
+
+
+#byte = Integer ('0b' + byte)
+#
+#puts "base 10: " + byte.to_s
+#puts "base 2: " + byte.to_s(2)
+#puts "Ascii Character: " + byte.chr
+
+
+
+
